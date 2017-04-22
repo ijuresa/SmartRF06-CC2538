cb9134f60b7623f28fb2c0f6eb96bdb657a774db
Ivan Juresa <ivanjuresa611@gmail.com>
2017-04-22 10:32:50 +0200

    .gitignore

--- Debug/cx2538_startup_ccs.d
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-cx2538_startup_ccs.obj: ../cx2538_startup_ccs.c
-cx2538_startup_ccs.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.2.LTS/include/stdint.h
-
-../cx2538_startup_ccs.c: 
-C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.2.LTS/include/stdint.h: 
