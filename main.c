780491c223e116ebe5cb144a465365a363b11c48
Ivan Juresa <ivanjuresa611@gmail.com>
2017-05-23 22:55:42 +0200

    * Updated main
    * Added UART for easier debugging and pin for one-wire protocol

--- main.c
+++ main.c
@@ -1,17 +1,79 @@
 
-#include <stdint.h>
-#include "aoa_driver.h"
+/*******************************************************************************
+ *                       INCLUDE FILES
+ ******************************************************************************/
+#include "main.h"
 
+/*******************************************************************************
+ *                       GLOBAL AND STATIC VARIABLES
+ ******************************************************************************/
 static AOA_plug_S aoaPlug;
 
+
+/*******************************************************************************
+* @brief    Program start
+*           Initialize clock, various peripherals
+********************************************************************************
+* @retval   Nothing
+*******************************************************************************/
 int main(void) {
     
+    // Clock
+    SysCtrlClockSet(false, false, SYS_CTRL_SYSDIV_32MHZ);
+
+    // I/O clock
+    SysCtrlIOClockSet(SYS_CTRL_SYSDIV_32MHZ);
+
+
+    // Initialize delay
+    INIT_delay(GPTIMER_B ,SYS_CTRL_PERIPH_GPT0, GPTIMER0_BASE,
+               TIMER_0B_CONFIGURATION);
+
+    // Initialize UART
+    INIT_uart();
+
+    // GPIO_PIN_1 is connected to LED1 (orange) -> Used only for testing
+    // GPIO_PIN_0 is connected to LED0 (red)
+    //      |
+    //      --> Removed jumper, used for AOA
+    // For reference: swru321b.pdf and swrr143.pdf
+    GPIOPinTypeGPIOOutput(GPIO_C_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+    GPIOPinWrite(GPIO_C_BASE, GPIO_PIN_0 | GPIO_PIN_1, 0);
+
+    GPIOPinWrite(GPIO_C_BASE, GPIO_PIN_0 | GPIO_PIN_1, GPIO_PIN_0 | GPIO_PIN_1);
+
+    UARTprintf("Test1212");
+
+
 
     for(;;) {
 
     }
 }
 
-/*********************************************************************************************************
+/*******************************************************************************
+* @brief    Set UART0 interface for console usage
+********************************************************************************
+* @retval   Nothing
+*******************************************************************************/
+void INIT_uart(void) {
+    // Transmit
+    IOCPinConfigPeriphOutput(GPIO_UART_BASE, UART_PIN_TX,
+                             IOC_MUX_OUT_SEL_UART0_TXD);
+
+    GPIOPinTypeUARTOutput(GPIO_UART_BASE, UART_PIN_TX);
+
+    // Receive
+    IOCPinConfigPeriphInput(GPIO_UART_BASE, UART_PIN_RX, IOC_UARTRXD_UART0);
+    GPIOPinTypeUARTInput(GPIO_UART_BASE, UART_PIN_RX);
+
+    // UART0
+    UARTStdioInit(0);
+}
+
+
+
+
+/*******************************************************************************
  *                       END OF FILE
- ********************************************************************************************************/
+ ******************************************************************************/
---
+++ main.h
@@ -0,0 +1,51 @@
+
+#ifndef MAIN_H_
+#define MAIN_H_
+/*******************************************************************************
+ *                       INCLUDE FILES
+ ******************************************************************************/
+#include <stdint.h>
+
+//! Low lvl
+#include "hw_memmap.h"
+#include "hw_ioc.h"
+
+//! Driver includes
+#include "sys_ctrl.h"
+#include "gpio.h"
+#include "uartstdio.h"
+#include "ioc.h"
+
+//! Local includes
+#include "aoa_driver.h"
+#include "delay.h"
+
+/*******************************************************************************
+ *                       CONSTANTS
+ ******************************************************************************/
+//! UART defines
+#define UART_PIN_RX     GPIO_PIN_0
+#define UART_PIN_TX     GPIO_PIN_1
+
+#define GPIO_UART_BASE  GPIO_A_BASE
+
+//! Timer
+#define TIMER_0B_CONFIGURATION  GPTIMER_CFG_SPLIT_PAIR      \
+                                | GPTIMER_CFG_A_ONE_SHOT    \
+                                | GPTIMER_CFG_B_ONE_SHOT
+
+//! Debug
+#define SERIAL_DEBUG    1
+
+/*******************************************************************************
+ *                       PUBLIC FUNCTION PROTOTYPES
+ ******************************************************************************/
+void INIT_uart(void);
+void INIT_timer0(void);
+
+
+
+#endif /* MAIN_H_ */
+/*******************************************************************************
+ *                       END OF FILE
+ ******************************************************************************/
